73707afb4236f931fd3d45851e6b4f5f87dd6410
zhangziqian <zhangziqian@sjtu.edu.cn>
2022-10-23 21:14:07 +0800

    应用系统体系架构 作业03

--- src/config/BaseConfig.js
+++ src/config/BaseConfig.js
@@ -1,12 +1,12 @@
 // 后端API的地址
-const apiURL = "http://localhost:8080";
+const apiURL = "https://localhost:8443";
 export {apiURL};
 
 // 阿里云对象存储 主机地址
 const picHost = "https://ebookpicture.oss-cn-hangzhou.aliyuncs.com";
 export {picHost}
 
-// 阿里云对象存储 AccessID
+// 阿里云对象存储用户名ID AccessID
 const accessId = "LTAI5tM9SVRoHcGY8meMzB2c";
 export {accessId};
 
